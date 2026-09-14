30a376682cb6fe8a42c7acb58aa25f6676e54dca
Abdelali Adrif <abdelaliadrif02@gmail.com>
2026-09-14 18:19:32 +0100

    [ADD]: add new module bst_l10n_ma_city

---
+++ bst_l10n_ma_city/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Maroc - Référentiel des villes',
+    'version': '19.0.1.0.0',
+    'category': 'Localization',
+    'summary': "Référentiel des villes et codes postaux du Maroc",
+    'description': """
+Charge le référentiel des villes du Maroc (res.city) avec code postal
+et région, et active la sélection de ville par liste déroulante sur
+les contacts marocains.
+    """,
+    'author': 'Blackswan Technology',
+    'depends': ['base_address_extended'],
+    'data': [
+       'data/res_country_data.xml',
+       'data/res_country_state_data.xml',
+       'data/res.city.csv',
+    ],
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
